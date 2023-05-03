9b87b5004b9a422f61d5df1c0dbb1ab0039c5cc2
pawelgorszczak <pawelgorszczakpl@gmail.com>
2023-05-03 21:59:18 +0200

    Hello world code example

---
+++ official-documentation-tutorials/1.Hello World/Receive/Receive.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() {
+  HostName = "rabbit-test-instance"  
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+  queue: "hello-world-queue",
+  durable: true,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null
+);
+
+System.Console.WriteLine("Waiting for messages");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    System.Console.WriteLine($"Received message - '{message}'");
+};
+
+channel.BasicConsume(
+    queue: "hello-world-queue",
+    autoAck: true,
+    consumer: consumer
+);
+
+System.Console.WriteLine("Press key to exit");
+System.Console.ReadLine();
---
+++ official-documentation-tutorials/1.Hello World/Send/Send.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {
+  HostName = "rabbit-test-instance"  
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+  queue: "hello-world-queue",
+  durable: true,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null
+);
+
+const string message = "Hello World! - first message in rabbit MQ";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+  exchange: string.Empty,
+  routingKey: "hello-world-queue",
+  basicProperties: null,
+  body: body
+);
+
+System.Console.WriteLine($"Message sent! - '{message}'");
